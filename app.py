339c6db776d3490062d90625acb2c57fe0616469
braganetowork <braga.neto.work@gmail.com>
2026-02-18 09:56:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import languagemodels as lm
 from fastapi import FastAPI, UploadFile, File
 app = FastAPI()
 
+
+def to_english( ptg: str): 
+    return lm.do(f"Translate to English the sentence: {ptg.capitalize()}") if ptg else "-"
+
 @app.get("/")
 async def read_root():
     return {"message": "hello"}
@@ -9,7 +13,7 @@ async def read_root():
 
 @app.get("/translate")
 async def read_root( msg: str):
-    return {"portuguese": msg, "english": lm.do(f"Translate to English: {msg}")}
+    return {"portuguese": msg, "english": to_english(msg)}
 
 
 @app.get("/answer")
@@ -22,14 +26,14 @@ async def read_root( msg: str):
     lm.store_doc(lm.get_wiki("Fortran"), "Fortran")
     lm.store_doc(lm.get_wiki("C language"), "C")
     lm.store_doc(lm.get_wiki("R language"), "R")
-    msg_english = lm.do(f"Translate to English: {msg.capitalize()}")
-    answer = lm.get_doc_context(msg_english)
-    return {"portuguese": msg, "english": msg_english, "answer": (answer? str(answer) : "-") }
+    msg_english = to_english(msg)
+    answer = lm.get_doc_context(msg_english) if len(msg_english) > 1 else "-" 
+    return {"portuguese": msg, "english": msg_english, "answer": answer }
 
 
 @app.get("/time")
 async def read_root( question: str = None):
-    question_english = lm.do(f"Translate to English: {question.capitalize()}") if question else "Good Morning or Good Night? I have no more questions!"
+    question_english = to_english(question) if question else "Good Morning or Good Night? I have no more questions!"
     output = lm.chat(f'''
                 System: Respond as a helpful assistant. Your response should take into account the date and time, which is {lm.get_date()}.
 
